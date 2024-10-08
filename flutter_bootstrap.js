5eb272e2492c85d6265331cadb97d9917f9be4f8
Joao Carlos <jcorreadeazevedo@gmail.com>
2024-10-08 12:07:20 -0400

    version 1.1.16 preview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3925791662"
+    serviceWorkerVersion: "4258752459"
   }
 });
